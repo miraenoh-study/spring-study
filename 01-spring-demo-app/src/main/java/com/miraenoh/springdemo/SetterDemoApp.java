ebabfd64196edf087616e05b0fe456864e54bb5c
miraenoh <mnoh@novamobility.com>
2020-03-11 11:07:52 -0700

    Added literal values into TennisCoach using setter DI

--- 01-spring-demo-app/src/main/java/com/miraenoh/springdemo/SetterDemoApp.java
+++ 01-spring-demo-app/src/main/java/com/miraenoh/springdemo/SetterDemoApp.java
@@ -14,6 +14,10 @@ public class SetterDemoApp {
         System.out.println(theCoach.getDailyWorkout());
         System.out.println(theCoach.getDailyFortune());
 
+        //  Call our new methods to get the literal values
+        System.out.println(theCoach.getEmailAddress());
+        System.out.println(theCoach.getTeam());
+
         //  Close the context
         context.close();
     }
--- 01-spring-demo-app/src/main/java/com/miraenoh/springdemo/TennisCoach.java
+++ 01-spring-demo-app/src/main/java/com/miraenoh/springdemo/TennisCoach.java
@@ -3,6 +3,9 @@ package com.miraenoh.springdemo;
 import com.miraenoh.springdemo.services.FortuneService;
 
 public class TennisCoach implements Coach {
+    private String emailAddress;
+    private String team;
+
     private FortuneService fortuneService;
 
     //  Create a no-arg constructor
@@ -10,7 +13,7 @@ public class TennisCoach implements Coach {
         System.out.println("CricketCoach: inside no-arg constructor");
     }
 
-    //  Our setter method for DI
+    //  Our setter methods for DI
     public void setFortuneService(FortuneService fortuneService) {
         System.out.println("CricketCoach: inside setter method - setFortuneService");
         this.fortuneService = fortuneService;
@@ -24,5 +27,23 @@ public class TennisCoach implements Coach {
     @Override
     public String getDailyFortune() {
         return fortuneService.getFortune();
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        System.out.println("CricketCoach: inside setter method - setEmailAddress");
+        this.emailAddress = emailAddress;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public void setTeam(String team) {
+        System.out.println("CricketCoach: inside setter method - setTeam");
+        this.team = team;
     }
 }
